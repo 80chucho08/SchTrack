cb8792dbd1d3d7dc15fcb81cc2aa2b70b0e8df88
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-12 22:55:55 -0600

    feat: estado y funcion para agregar materias al array de estados

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -3,9 +3,36 @@ import type { Materia } from '../types/schedule';
 
 const SchedulePage = () => {
     const [materias, setMaterias] = useState<Materia[]>([]);
+    const [nombreMateria, setNombreMateria] = useState("");
+
+    const handleAgregarMateria = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (nombreMateria.trim() === "") return;
+
+        const nuevaMateria: Materia = {
+            id: crypto.randomUUID(),
+            nombre: nombreMateria.trim(),
+        };
+
+        setMaterias((prev) => [...prev, nuevaMateria]);
+        setNombreMateria("");
+    }
     return (
         <div>
             <h1>Mi horario</h1>
+            <form onSubmit={handleAgregarMateria}>
+                <input 
+                    type="text" 
+                    placeholder='Nombre de la materia'
+                    value={nombreMateria}
+                    onChange={(e) => setNombreMateria(e.target.value)}
+                />
+
+                <button type='submit'>Agregar</button>
+            </form>
+
+
             <h2>Materias</h2>
 
             <ul>
