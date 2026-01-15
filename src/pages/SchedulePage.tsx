2125f37a8f93a8d91fa5b497bd1a223e692ec9fc
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-14 22:24:56 -0600

    fix: dejar una card al poner una materia y poder volverla a usar para cambiar de celda cuando se vuelve a ocupar eliminando la primera

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -11,6 +11,13 @@ const SchedulePage = () => {
         const stored = localStorage.getItem("materias");
         return stored ? JSON.parse(stored) : [];
     });
+
+    const findMateriaCell = (materiaId: string) => {
+        return Object.entries(horario).find(
+            ([_, materia]) => materia.id === materiaId
+        )?.[0];
+    };
+
     const [horario, setHorario] = useState<HorarioState>(() => {
         const stored = localStorage.getItem("horario");
         return stored ? JSON.parse(stored) : {};
@@ -43,26 +50,31 @@ const SchedulePage = () => {
         if (!over) return;
 
         const materiaId = active.id as string;
-        const celdaId = over.id as string;
-
-        //para no sorbe escribir una celda ya ocupada
-        if (horario[celdaId]) return;
+        const targetCellId = over.id as string;
 
         const materia = materias.find((m) => m.id === materiaId);
         if (!materia) return;
 
-        // colocar la materia en el horario
-        setHorario((prev) => ({
-            ...prev,
-            [celdaId]: materia,
-        }));
+        setHorario((prev) => {
+            const newHorario = { ...prev };
 
-        // quitar materia del panel
-        setMaterias((prev) => prev.filter((m) => m.id !== materiaId));
+            // buscar si ya estaba colocada
+            const previousCell = Object.entries(prev).find(
+                ([_, m]) => m.id === materiaId
+            )?.[0];
 
-        console.log("Materia arrastrada", active.id);
-        console.log("Destino: ", over?.id);
+            // si estaba en otra celda, eliminarla
+            if (previousCell) {
+                delete newHorario[previousCell];
+            }
+
+            // colocar en la nueva celda
+            newHorario[targetCellId] = materia;
+
+            return newHorario;
+        });
     };
+
     return (
         <div>
             <h1>Mi horario</h1>
